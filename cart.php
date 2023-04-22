3b082f5b9ee9cc864a6408d056115e43d3c74755
Nishchay <nishchayr@iitbhilai.ac.in>
2023-04-22 20:07:37 +0530

    asd

---
+++ cart.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <link rel="stylesheet" href="cart.css">
+</head>
+
+<body>
+    <nav id="navigationbar">
+        <ul id="Menubars">
+            <li><a href="home.html">HOME</a></li>
+            <li><a href="cart.php">CART</a></li>
+            <li><a href="order.php">ORDER LIST</a></li>
+            <li><a href="account.php">ACCOUNT</a></li>
+        </ul>
+    </nav>
+
+    <div>
+        
+    </div>
+
+</body>
+</html>
