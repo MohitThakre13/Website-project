faa352e082698875541c8b851809ce25dad62d97
Nishchay <nishchayr@iitbhilai.ac.in>
2023-04-23 20:05:13 +0530

    signup done

--- test.php
+++ test.php
@@ -7,7 +7,21 @@
             echo  $f*2;
             echo "</p>" ;
             
+            $hello=5 ;
+
+            if(isset($_POST["submit"])){$hello = $_POST["number"]; }
+            echo $hello ;
+            
              
         ?>
+
+        <form aciton="test.php" method="post">
+            <input type="number" name="number" value="<?php echo $hello; ?>">
+            <input type="submit" name="submit" >
+            
+        </form>
+
+       
+
     </body>
 </html>
