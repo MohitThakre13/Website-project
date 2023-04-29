f0361883f8ca40b5ecd127f0a663afc56795ad57
Mohit <mohitt@iithbilai.ac.in>
2023-04-29 15:33:11 +0530

    hlo

--- order.php
+++ order.php
@@ -1,57 +1,30 @@
 <html>
-    <head>
-            <title>Order List</title>
-            <link rel="stylesheet" href="cart.css">
-    </head>
+        <head>
+                <title>Order List</title>
+        </head>
 
-    <body>
-
-    <nav id="navigationbar">
-        <ul id="Menubars">
-            <li><a href="home.html">HOME</a></li>
-            <li><a href="cart.php">CART</a></li>
-            <li><a href="order.php">ORDER LIST</a></li>
-            <li><a href="account.php">ACCOUNT</a></li>
-        </ul>
-    </nav>
-
-    <div style="margin-top:50px ;" > 
-        <table class="table" style="width:100%">
-            <thead>
-                <tr>
-
-                    <th scope="col">Product</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Each Price</th>
-                    <th scope="col">Total Price</th>
-                </tr>
-            </thead>
-            <tbody style="text-align: center;">
- 
- <?php   
-                chdir('nischayr');
-                $file = fopen("cartcount.txt", "r");
-                echo "<form action=\"checkout.php\" method=\"post\">"; 
-                while (!feof($file)) {
-                    $product = fgets($file);
-                    $eachprice = fgets($file);
-                    $quantity = fgets($file);
-                    $eachprice = rtrim($eachprice, "\n");
-                    $quantity = rtrim($quantity, "\n");
-                    $product = rtrim($product, "\n");
-                    $total = (int)$eachprice * (int)$quantity;
-                    if ($total != 0) {
-                        echo "<tr style=\"height: 100px;\">
-                <td>$product</td>
-                <td><input type=\"number\" class=\"quantity\" name=\"$product\" value=\"$quantity\"></td>
-                <td>$eachprice</td>
-                <td>$total</td>
-              </tr>";
-                    }
+        <body>
+                <form method="post" action="orderlist.php">
+<?php
+                $user="";
+                 $file_tmp = fopen ("username.txt","r");
+                 while (($m=fgetc($file_tmp)) != "\n") $user=$user.$m;
+                 fclose($file_tmp);
+                 chdir($user);
+                 
+         
+                $handle=opendir('./');
+                $count=0;
+                while (false !== ($entry = readdir($handle))) {
+                    $count++;
                 }
-                echo "</form>" ;
+                $count=$count-4;
+                while ($count != 0){
+                    echo "<input type=\"submit\" name=\"Order\" value=\"Order $count\"><br>";
+                    $count--;
+                }
+                closedir($handle);
 ?>
-    </tbody>
-    </div>
-    </body>
+                </form>
+        </body>
 </html>
--- orderlist.php
+++ orderlist.php
@@ -1,19 +1,62 @@
 <html>
-        <head>
-                <title>Order List</titile>
-        </head>
+    <head>
+            <title>Order List</title>
+            <link rel="stylesheet" href="cart.css">
+    </head>
 
-        <body>
-                <form>
-<?php
-                 $user="";
-                 $file_tmp = fopen ("username.txt","r");
-                 while (($m=fgetc($file_tmp)) != "\n") $user=$user.$m;
-                 fclose($file_tmp);
-                 chdir($user);
-         
-                $m=readdir();
+    <body>
+
+    <nav id="navigationbar">
+        <ul id="Menubars">
+            <li><a href="home.html">HOME</a></li>
+            <li><a href="cart.php">CART</a></li>
+            <li><a href="order.php">ORDER LIST</a></li>
+            <li><a href="account.php">ACCOUNT</a></li>
+        </ul>
+    </nav>
+
+    <div style="margin-top:50px ;" > 
+        <table class="table" style="width:100%">
+            <thead>
+                <tr>
+
+                    <th scope="col">Product</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Each Price</th>
+                    <th scope="col">Total Price</th>
+                </tr>
+            </thead>
+            <tbody style="text-align: center;">
+ 
+ <?php   
+ $name=ltrim($_POST['Order'],"Order ");
+                chdir('nischayr');
+                $file = fopen($name, "r");
+                echo "<form>"; 
+                while (!feof($file)) {
+                    $product = fgets($file);
+                    $eachprice = fgets($file);
+                    $quantity = fgets($file);
+                    $eachprice = rtrim($eachprice, "\n");
+                    $quantity = rtrim($quantity, "\n");
+                    $product = rtrim($product, "\n");
+                    $total = (int)$eachprice * (int)$quantity;
+                    if ($product == "$"){
+                        $address = fgets($file);
+                        echo "<br>".$address;
+                    }
+                    if ($total != 0) {
+                        echo "<tr style=\"height: 100px;\">
+                <td>$product</td>
+                <td>$quantity</td>
+                <td>$eachprice</td>
+                <td>$total</td>
+              </tr>";
+                    }
+                }
+                echo "</form>" ;
 ?>
-                </form>
-        </body>
+    </tbody>
+    </div>
+    </body>
 </html>
